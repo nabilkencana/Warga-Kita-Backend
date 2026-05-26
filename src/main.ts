20d99de16eb421ced73d7a31bbd4516547d46c8b
nabilkencana <nabilkencana20@gmail.com>
2026-05-26 07:49:02 +0700

    chore: update CORS policy to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
   // Enable CORS untuk semua origin (development only)
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:8080', 'https://wargakita.canadev.my.id', 'http://localhost:57609'],
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
